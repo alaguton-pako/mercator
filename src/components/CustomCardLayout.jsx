4705c6ea7353ed669364a3dc75e00afd89abd9b7
Ahmed <lameda789@gmail.com>
2025-05-24 13:41:44 +0100

    responsivness fix

--- src/components/CustomCardLayout.jsx
+++ src/components/CustomCardLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CustomCardLayout = ({ children }) => {
-return <div className="p-6 bg-[#F8FBFF]">{children}</div>;
+return <div className="p-6 bg-[#F8FBFF] h-full">{children}</div>;
 };
 
 export default CustomCardLayout;
